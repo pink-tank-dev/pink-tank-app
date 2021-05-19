db53defe0faf532246ce6abfc43a2eea209cb756
Nazisagit <pgnazrin@gmail.com>
2021-05-19 18:07:27 +0800

    19052021 disable auto generate helper and assets, set time zone, english locale

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,18 @@ module PinkTankApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.generators do |g|
+      g.helper false
+      g.assets false
+    end
+    config.time_zone = 'Asia/Kuala_Lumpur'
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
