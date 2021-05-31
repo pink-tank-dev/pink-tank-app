983ad62015a3dcced5ecd8b83996bd6f80298d6c
Nazisagit <pgnazrin@gmail.com>
2021-05-31 23:32:46 +0800

    31052021 create users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,71 @@
+class UsersController < ApplicationController
+  before_action :set_user, except: %i[index new create]
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_params)
+    if @user.valid?
+      @user.save
+      redirect_to @user, success: "#{@user.name} successfully created."
+    else
+      flash[:danger] = user_errors
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def edit_password; end
+
+  def update
+    @user.assign_attributes(update_params)
+    if @user.valid?
+      @user.save
+      redirect_to @user, success: "#{@user.name} successfully updated."
+    else
+      flash[:danger] = user_errors
+      render :edit
+    end
+  end
+
+  def update_password
+    @user.assign_attributes(update_params)
+    if @user.valid?
+      @user.save
+      redirect_to @user, success: "Password successfully updated."
+    else
+      flash[:danger] = user_errors
+      render :edit
+    end
+  end
+
+  def show; end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def update_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  def update_password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def user_errors
+    @user.errors.full_messages.join(', ')
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  has_secure_password
+  validates :name, :email, presence: true
+end
