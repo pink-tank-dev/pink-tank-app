5f7dc1e79484d34eb8c4f2bee5d7bba1d07be1e2
Nazisagit <pgnazrin@gmail.com>
2021-06-07 00:03:51 +0800

    07062021 added logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,7 +18,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session.delete(:user_id)
+    session.clear
+    @current_user = nil
     redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  get 'dashboard', to: 'dashboard#show'
+  resources :users, except: %i[destroy] do
+    member do
+      get 'edit_password'
+      put 'update_password'
+    end
+  end
+  resources :artists, except: %i[destroy]
+
+  draw :web
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
