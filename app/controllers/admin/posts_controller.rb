e46bf4fa3106fbf2d4c15882c0ee082b51f263ba
Nazisagit <pgnazrin@gmail.com>
2021-07-16 16:22:25 +0800

    16072021 moved controllers to admin module

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,81 @@
+module Admin
+  class PostsController < ApplicationController
+    before_action :set_artist
+    before_action :set_post, only: %i[edit update show]
+
+    PUBLISH = "Publish".freeze
+    UNPUBLISH = "Unpublish".freeze
+
+    def index
+      redirect_to admin_artists_path, warning: "Artist not found." unless @artist.present?
+      @posts = @artist.posts.with_rich_text_body.order(created_at: :asc)
+    end
+
+    def new
+      @post = Post.new
+    end
+
+    def create
+      @post = Post.new(post_params.merge(artist_id: @artist.id))
+
+      @post.assign_attributes(status: :published, published_at: Time.current) if publishing?
+
+      if @post.save
+        _path = @post.draft? ? admin_artist_post_path(@artist, @post) : admin_artist_posts_path(@artist)
+        message = @post.draft? ? "created" : "published"
+        redirect_to _path, success: "#{@post.title} successfully #{message}."
+      else
+        flash[:danger] = post_errors
+        render :new
+      end
+    end
+
+    def edit; end
+    
+    def update
+      if unpublishing?
+        @post.assign_attributes(status: :draft, published_at: nil)
+      else
+        @post.assign_attributes(post_params)
+        @post.assign_attributes(status: :published, published_at: Time.current) if publishing?
+      end
+
+      if @post.save
+        _path = @post.draft? ? admin_artist_post_path(@artist, @post) : admin_artist_posts_path(@artist)
+        message = @post.draft? ? "updated" : "published"
+        redirect_to _path, success: "#{@post.title} successfully #{message}."
+      else
+        flash[:danger] = post_errors
+        render :edit
+      end
+    end
+
+    def show; end
+
+    private
+
+    def set_artist
+      @artist = Artist.find_by(id: params[:artist_id])
+    end
+
+    def set_post
+      @post = Post.find_by(id: params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :body, :status)
+    end
+
+    def post_errors
+      @post.errors.full_messages.join(', ')
+    end
+
+    def publishing?
+      params[:commit] == PUBLISH
+    end
+
+    def unpublishing?
+      params[:commit] == UNPUBLISH
+    end
+  end
+end
---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,77 @@
+module Admin
+  class UsersController < ApplicationController
+    before_action :set_user, except: %i[index new create]
+    def index
+      @users = User.all
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new(create_params)
+      if @user.save
+        redirect_to admin_user_path(@user), success: "#{@user.name} successfully created."
+      else
+        flash[:danger] = user_errors
+        render :new
+      end
+    end
+
+    def edit
+      redirect_to users_path, warning: "User not found." unless @user.present?
+    end
+
+
+    def update
+      @user.assign_attributes(update_params)
+      if @user.save
+        redirect_to admin_user_path(@user), success: "#{@user.name} successfully updated."
+      else
+        flash[:danger] = user_errors
+        render :edit
+      end
+    end
+
+    def edit_password
+      redirect_to users_path, warning: "User not found." unless @user.present?
+    end
+
+    def update_password
+      @user.assign_attributes(update_params)
+      if @user.save
+        redirect_to admin_user_path(@user), success: "Password successfully updated."
+      else
+        flash[:danger] = user_errors
+        render :edit
+      end
+    end
+
+    def show
+      redirect_to admin_users_path, warning: "User not found." unless @user.present?
+    end
+
+    private
+
+    def create_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def update_params
+      params.require(:user).permit(:name, :email)
+    end
+
+    def update_password_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def set_user
+      @user = User.find_by(id: params[:id])
+    end
+
+    def user_errors
+      @user.errors.full_messages.join(', ')
+    end
+  end
+end
