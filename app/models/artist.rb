3d1f1a4b3db4bd6dd4ce21dd0d60b729a8f9ee3d
Nazisagit <pgnazrin@gmail.com>
2021-07-17 12:54:41 +0800

    17072021 add password_digest to artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,13 @@
 #
 # Table name: artists
 #
-#  id         :bigint           not null, primary key
-#  about      :text
-#  email      :string
-#  name       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :bigint           not null, primary key
+#  about           :text
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
 #
 class Artist < ApplicationRecord
   has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_084148) do
+ActiveRecord::Schema.define(version: 2021_07_16_094759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_07_16_084148) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "about"
     t.string "email"
+    t.string "password_digest"
   end
 
   create_table "posts", force: :cascade do |t|
