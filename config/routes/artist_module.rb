71bbee1f9ea84122ad773d9d75c0d90eb4452dfd
Nazisagit <pgnazrin@gmail.com>
2021-07-17 13:13:09 +0800

    17072021 add profile to routes

--- config/routes/artist_module.rb
+++ config/routes/artist_module.rb
@@ -1,5 +1,6 @@
-namespace :artist do
+namespace :artist_module, path: 'artist' do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  resources :posts
+  resource :profile, except: %i[new create destroy]
+  resources :posts, except: %i[index destroy]
 end
