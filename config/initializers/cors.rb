e2327b5a8b62c67a041336f289b72ca29b47f686
Nazisagit <pgnazrin@gmail.com>
2021-07-20 14:28:27 +0800

    20072021 add cors for react staging and production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'http://localhost:8080'
-    resource '/api/*',
-      headers: :any,
-      methods: [:get, :post, :patch, :put]
+  if Rails.env.staging?
+    allow do
+      origins 'https://pink-tank-react-staging.herokuapp.com'
+      resource '/api/*',
+        headers: :any,
+        methods: [:get, :post, :patch, :put]
+    end
+  end
+
+  if Rails.env.production?
+    allow do
+      origins 'https://pink-tank-react-production.herokuapp.com'
+      resource '/api/*',
+        headers: :any,
+        methods: [:get, :post, :patch, :put]
+    end
   end
 end
