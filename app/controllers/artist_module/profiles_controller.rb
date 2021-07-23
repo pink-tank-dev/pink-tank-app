8a5cd8102928bc2177b482bae3abd1126e8fc216
Nazisagit <pgnazrin@gmail.com>
2021-07-23 22:33:03 +0800

    23072021 add ig, about, and statement to forms and profile page

--- app/controllers/artist_module/profiles_controller.rb
+++ app/controllers/artist_module/profiles_controller.rb
@@ -1,6 +1,7 @@
 module ArtistModule
   class ProfilesController < ::ApplicationController
     before_action :set_artist, only: %i[edit update]
+
     def show
       @posts = current_artist.posts.with_rich_text_body.order(created_at: :asc)
     end
@@ -24,7 +25,7 @@ module ArtistModule
     end
 
     def artist_params
-      params.require(:artist).permit(:name, :email, :about)
+      params.require(:artist).permit(:name, :email, :instagram, :about, :statement)
     end
   end
 end
