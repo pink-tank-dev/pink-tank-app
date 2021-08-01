7a14a41705afe2e144f0c0c724b9e4610911a81d
Nazisagit <pgnazrin@gmail.com>
2021-08-01 22:32:21 +0800

    01082021 use bootstrap min

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,8 +10,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap/dist/js/bootstrap"
-import "bootstrap/dist/css/bootstrap"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "trix/dist/trix.css";
 import "trix/dist/trix.js";
 
