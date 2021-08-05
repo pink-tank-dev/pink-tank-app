a8cc825155a969fb3b64cab874cdb9d167ab34a5
Nazisagit <pgnazrin@gmail.com>
2021-08-05 12:01:36 +0800

    05082021 refactor spec structure

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
+  describe "relations" do
+    it { is_expected.to have_many(:posts) }
+  end
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:email) }
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "validations" do
-    it { is_expected.to define_enum_for(:status).with_values(%i[draft posted]) }
-    it { is_expected.to validate_presence_of(:title) }
+  describe "relations" do
     it { is_expected.to belong_to(:artist) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:title) }
+  end
+
+  describe "enums" do
+    it { is_expected.to define_enum_for(:status).with_values(%i[draft posted]) }
   end
 end
