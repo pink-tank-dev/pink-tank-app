9ab5c13ec8053c90deea59c1a012c5864a6bb112
Nazisagit <pgnazrin@gmail.com>
2021-08-07 17:47:01 +0800

    07082021 restrict video uploads to 10mb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,18 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-// require("trix")
-require("@rails/actiontext")
-import "controllers"
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "bootstrap/dist/js/bootstrap.min.js"
-import "bootstrap/dist/css/bootstrap.min.css"
+require("@rails/actiontext");
+import "controllers";
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "trix/dist/trix.css";
 import "trix/dist/trix.js";
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
---
+++ app/javascript/packs/trix-editor-overrides.js
@@ -0,0 +1,8 @@
+window.addEventListener("trix-file-accept", function(event) {
+  const maxFileSize = 1024 * 10240;
+  const file = event.file
+  if (file.size > maxFileSize && !file.type.match(/video/).length == 0) {
+    event.preventDefault();
+    alert("Currently we only support video uploads of up to 10MB.");
+  }
+})
