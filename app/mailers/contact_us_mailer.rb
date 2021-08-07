dac5405c76f0f0d4ff7c24bfba8ef363121f4c95
Nazisagit <pgnazrin@gmail.com>
2021-08-07 08:32:01 +0800

    07082021 refactor contact us mailer

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -1,9 +1,10 @@
 class ContactUsMailer < ApplicationMailer
+  default template_path: 'mailers/contact_us_mailer'
+
   def send_email(name:, email:, subject:, message:)
     @message = message
     mail(reply_to: email_address_with_name(email, name),
          subject: subject,
-         template_path: 'mailers/contact_us_mailer',
-         template_name: 'contact_us')
+         template_name: 'email')
   end
 end
