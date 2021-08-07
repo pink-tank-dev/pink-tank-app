83d8a9d38a03e7d299c7b616f0d73aec58f74f7a
Nazisagit <pgnazrin@gmail.com>
2021-08-07 18:26:32 +0800

    07082021 add pdf embed

--- config/initializers/html_sanitization.rb
+++ config/initializers/html_sanitization.rb
@@ -1,4 +1,9 @@
 Rails::Html::WhiteListSanitizer.allowed_tags << "iframe"
 Rails::Html::WhiteListSanitizer.allowed_tags << "video"
 Rails::Html::WhiteListSanitizer.allowed_tags << "source"
+Rails::Html::WhiteListSanitizer.allowed_tags << "audio"
+Rails::Html::WhiteListSanitizer.allowed_tags << "embed"
+
+Rails::Html::WhiteListSanitizer.allowed_attributes << "poster"
+Rails::Html::WhiteListSanitizer.allowed_attributes << "style"
 Rails::Html::WhiteListSanitizer.allowed_attributes << "controls"
