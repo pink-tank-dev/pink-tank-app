3959319738d0d3cea1b5d25bd1c440f64c3dd91d
Nazisagit <pgnazrin@gmail.com>
2021-08-19 12:23:26 +0800

    19082021 increase video file size acceptance

--- app/javascript/packs/trix-editor-overrides.js
+++ app/javascript/packs/trix-editor-overrides.js
@@ -1,8 +1,8 @@
 window.addEventListener("trix-file-accept", function(event) {
-  const maxFileSize = 1024 * 10240;
+  const maxFileSize = 1024 * 20480;
   const file = event.file
   if (file.size > maxFileSize && !file.type.match(/video/).length == 0) {
     event.preventDefault();
-    alert("Currently we only support video uploads of up to 10MB.");
+    alert("Currently we only support video uploads of up to 20MB.");
   }
 })
