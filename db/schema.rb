c7cdd64f24b26dcaf917a9a2e3473a3db4fd94ab
Nazisagit <pgnazrin@gmail.com>
2021-09-26 17:49:31 +0800

    26092021 add position to artwork

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_163939) do
+ActiveRecord::Schema.define(version: 2021_09_26_090103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_163939) do
     t.bigint "artist_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "position"
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
