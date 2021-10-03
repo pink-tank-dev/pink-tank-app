986a8c5a24c9dcb9fb62ce0f3e5037acc697fffb
Nazisagit <pgnazrin@gmail.com>
2021-10-03 16:25:52 +0800

    03102021 add notes to artwork

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_03_074720) do
+ActiveRecord::Schema.define(version: 2021_10_03_081315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_10_03_074720) do
     t.string "measurements"
     t.string "medium"
     t.integer "status"
+    t.text "notes"
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
