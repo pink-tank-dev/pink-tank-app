e98939d26ef3e19705e85590f52a230beae744b3
Nazisagit <pgnazrin@gmail.com>
2021-10-03 22:05:06 +0800

    03102021 add price to artwork model

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -8,6 +8,7 @@
 #  medium       :string
 #  notes        :text
 #  position     :integer
+#  price_cents  :integer          default(0), not null
 #  status       :integer
 #  title        :string
 #  created_at   :datetime         not null
@@ -29,6 +30,8 @@ class Artwork < ApplicationRecord
   validates :medium, :measurements, :status, presence: true
 
   enum status: %i[available sold]
+
+  monetize :price_cents
 
   scope :by_artist, -> (id) { where(artist_id: id) }
 end
