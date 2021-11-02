ccd24d8cb6bc7317d625107df1c7d9bed52ccb47
Nazisagit <pgnazrin@gmail.com>
2021-11-02 22:16:17 +0800

    02112021 set asset host for whole app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_controller.asset_host = "d248gssp7wven8.cloudfront.net"
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
@@ -35,7 +34,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = 'http://assets.example.com'
+  config.asset_host = "d248gssp7wven8.cloudfront.net"
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
