db687f00f0910e14176fdc4fb68450b90a7e852c
Nazisagit <pgnazrin@gmail.com>
2021-11-08 00:17:18 +0800

    08112021 used post body instead of corrected_html

--- app/models/post.rb
+++ app/models/post.rb
@@ -19,25 +19,4 @@ class Post < ApplicationRecord
   validates :title, presence: true
   has_rich_text :body
   belongs_to :artist
-
-  def corrected_html
-    html = body.to_s
-    doc = Nokogiri::HTML(html)
-    attachments = doc.css("action-text-attachment")
-    return doc.to_s unless attachments.present?
-    correct_media_src_url(attachments, "img")
-    correct_media_src_url(attachments, "video")
-    correct_media_src_url(attachments, "audio")
-    correct_media_src_url(attachments, "embed")
-    doc.to_s
-  end
-
-  def correct_media_src_url(attachments, media_type)
-    attachments.each do |node|
-      url = node["url"]
-      media = node.css(media_type).first
-      next unless media.present?
-      media["src"] = url
-    end
-  end
 end
