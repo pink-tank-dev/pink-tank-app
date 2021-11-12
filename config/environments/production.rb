326a3b1f58d3db0cf3cc80715ff4660ddf764915
Nazisagi <pgnazrin@gmail.com>
2021-11-12 23:26:40 +0800

    12112021 remove default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,4 @@
 require "active_support/core_ext/integer/time"
-Rails.application.default_url_options = { 
-  host: "pink-tank-app-#{Rails.env}.herokuapp.com",
-  protocol: "https"
-}
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
