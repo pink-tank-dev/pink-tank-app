4f428172f0fcc91495da545e526e123f4cb6d93c
Nazisagi <pgnazrin@gmail.com>
2021-11-30 18:46:41 +0800

    30112021 refactor contact us spec

--- spec/requests/api/v1/contact_us/create_spec.rb
+++ spec/requests/api/v1/contact_us/create_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "Api::ContactUs", type: :request do
+RSpec.describe "Api::V1::ContactUs", type: :request do
   describe "POST /create" do
     let(:params) do
       {
